7a89c53c829dd40889b8b9f346f021e9f5fb163d
Pavlo Ocheretianyi <pavel_ocheretyany_01@mail.ua>
2021-09-09 19:35:08 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace dzshka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите N (кол. раз): ");
+            string numb_N = Console.ReadLine();
+            Console.Write("Введите M (длину): ");
+            string numb_M = Console.ReadLine();
+            if (numb_N.All(char.IsDigit) && numb_M.All(char.IsDigit))
+            {
+                int x = Int32.Parse(numb_N);
+                int y = Int32.Parse(numb_M);
+
+                for (int i = 1; i <= (x * y); i++)
+                {
+                    Console.WriteLine("█\n");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Стока должна быть только intовой...");
+            }
+
+            // Без проверки на число
+            /*Console.Write("Введите N (кол. раз): ");
+            int numb_N = Int32.Parse(Console.ReadLine());
+            Console.Write("Введите M (длину): ");
+            int numb_M = Int32.Parse(Console.ReadLine());
+            for (int i = 1; i <= (numb_M * numb_N); i++)
+            {
+                Console.WriteLine("█\n");
+            }*/
+        }
+    }
+}
